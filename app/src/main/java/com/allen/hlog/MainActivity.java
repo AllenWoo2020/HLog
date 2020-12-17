120f7a7d4ca3f857ea16eb7de0462e39a3d87384
i <wuhaoscholar@163.com>
2020-12-17 17:57:21 +0800

    cuo

--- app/src/main/java/com/allen/hlog/MainActivity.java
+++ app/src/main/java/com/allen/hlog/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.allen.hao.Hao;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Hao.init(getApplication(),true);
+//        Hao.e("1212121212");
     }
 }
